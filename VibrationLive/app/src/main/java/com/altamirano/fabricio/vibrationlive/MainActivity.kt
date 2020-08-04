88ef63b0b36744313b488ef3fc2eda528b582d78
Fabricio Altamirano <fabricio.altamirano@exaccta.com>
2020-08-04 10:01:24 +0200

    fix main

--- VibrationLive/app/src/main/java/com/altamirano/fabricio/vibrationlive/MainActivity.kt
+++ VibrationLive/app/src/main/java/com/altamirano/fabricio/vibrationlive/MainActivity.kt
@@ -1,22 +1,11 @@
 package com.altamirano.fabricio.vibrationlive
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Vibrator
-import android.util.Log
-import android.view.MotionEvent
-import android.view.View
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.altamirano.fabricio.vibrationlive.fragments.RecordVibrationFragment
 import com.altamirano.fabricio.vibrationlive.fragments.SingleVibrationFragment
-import com.altamirano.fabricio.vibrationlive.models.DataVibrate
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var vibrator: Vibrator
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,16 +14,6 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
 
         inflateFragment(SingleVibrationFragment())
-
-
-      /*  vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-
-        btnVibrate = findViewById(R.id.btnVibrate)
-
-        btnVibrate.setOnClickListener { this.onVibrate() }
-
-        findViewById<View>(R.id.content).setOnTouchListener(this)
-        findViewById<View>(R.id.btnPlay).setOnClickListener { this.onPlayRecord() }*/
     }
 
     fun inflateFragment(fragment:Fragment){
--- VibrationLive/app/src/main/java/com/altamirano/fabricio/vibrationlive/service/ModelService.kt
+++ VibrationLive/app/src/main/java/com/altamirano/fabricio/vibrationlive/service/ModelService.kt
@@ -6,7 +6,8 @@ data class CodeResponse(var status:Int,var code:String)
 
 data class PairResponse(var status:Int,var with_user:String?, var connected:Int?=null, var is_active:Int?=null)
 
-data class PairDeviceResponse(var status:Int,var with_user:String, var code: String)
+data class PairDeviceResponse(var status:Int, var with_user:String, var code: String)
 
 data class MessageRequest(var code:String,var is_active:Int)
+
 data class MessageResponse(var status:Int)
